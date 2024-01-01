d164550f6443fd4d2b0b03d9f41f1dfbc46603e0
marekdrab <marek.drablp@gmail.com>
2024-01-02 00:08:51 +0100

    code cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlanetController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
